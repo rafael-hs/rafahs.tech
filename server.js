2b542eae31e2a06617813e4a11369d37eb0372ce
Rafael Honório <rafahs.tech@gmail.com>
2022-01-23 22:13:46 -0300

    add heroku config

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/rafahs-tech'));
+app.use(express.static(__dirname + '/dist/rafahs-tech'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/rafahs-tech/'}),
+    res.sendFile(paht.join(__dirname + "dist/rafahs-tech/index.html")),
 );
 
 // Start the app by listening on the default Heroku port
