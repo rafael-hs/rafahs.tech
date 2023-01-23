b0bd7bbcdfa8acc5d0e3cfbeddf68f6754f6ef7c
Rafael Honório <rafahs.tech@gmail.com>
2023-01-22 23:56:26 -0300

    change server.js config

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist/rafahs-tech'));
 
 app.get('/*', (req, res) =>
-    res.sendFile(paht.join(__dirname + "dist/rafahs-tech/index.html")),
+    res.sendFile(path.join(__dirname + "dist/rafahs-tech/index.html")),
 );
 
 // Start the app by listening on the default Heroku port
